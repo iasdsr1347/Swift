460958901dd6494c230c012a528295069492356b
Song Young Min <iasdsr1347@naver.com>
2025-12-09 01:43:35 +0900

    [FEAT] if let, guard let 사용 및 차이점

---
+++ 00. iOS-swift/01-swift-basics2/01-swift-basics2/main.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  01-swift-basics2
+//
+//  Created by 송영민 on 12/9/25.
+//
+
+import Foundation
+
+var nickname: String? = nil
+
+// if let 옵셔널 바인딩
+// nickname이 nil이 아니면 값을 추출해서 nick 상수에 저장
+if let nick = nickname {
+    print("별명: \(nick)")
+} else {
+    print("별명 없음")
+} // if문 안에서만 nick 사용 가능 , nil이면 else 블록 실행
+
+
+// guard let 조기 종료 패턴
+// nil이면 else구문 실행
+// 이 시점: text는 String? (옵셔널)
+func printUppercased(_ text: String?) {
+    // 오른쪽 text: 함수 파라미터 (String? 타입, 옵셔널)
+    // 왼쪽 text: 새로 만드는 상수 (String 타입, 언래핑됨)
+    guard let text = text else {
+        print("값이 없음")
+        return //반드시 return/throw/break/continue 등으로 스코프 탈출
+    }
+    // 이 시점: text는 String (일반 타입)
+    // 위의 옵셔널 text를 가리던 이름이 새로운 String text로 덮어씌워짐
+    print(text.uppercased())
+}
+
+printUppercased("hello")
+printUppercased(nil)
+
+
+
+// 1. 라벨 생략 (_)
+func add(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+add(1, 2)  // ✅
+
+// 2. 라벨 사용
+func add(a: Int, b: Int) -> Int {
+    return a + b
+}
+add(a: 1, b: 2)  // ✅
+
+// 3. 외부/내부 라벨 다르게
+func add(first a: Int, second b: Int) -> Int {
+    return a + b
+}
+add(first: 1, second: 2)  // 호출 시: first, second 사용
+// 함수 내부에서는: a, b 사용
+
+/*
+// 함수이름 자체가 의마가 부족하면, 파라미터 레이블 사용하기
+func move(from: String, to: String)  // 레이블이 있어야 명확함
+move(from: "서울", to: "부산")
+*/
